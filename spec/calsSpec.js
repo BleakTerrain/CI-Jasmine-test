198856b4e6b77fc20d125866971bd4fc52cd641d
Ubuntu <ubuntu@ip-172-31-80-86.ec2.internal>
2020-05-10 09:21:00 +0000

    second commit with initial files

---
+++ spec/calsSpec.js
@@ -0,0 +1,10 @@
+describe("Calculator", function()
+{
+    describe("Addition test", function()
+    {
+        it("should return 42", function()
+        {
+            expect(addition(20, 22)).toBe(42);
+        });
+    });
+});
